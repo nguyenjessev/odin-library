53cc966560d015f918af02e180883f49a3e6d4a8
Jesse Nguyen <nguyenjessev@gmail.com>
2022-05-05 19:53:52 -0400

    Add refreshBooks function

--- library.js
+++ library.js
@@ -7,7 +7,21 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
   newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  refreshBooks();
+}
+
+function refreshBooks() {
+  document.querySelector('.library-table tbody').innerHTML = null
+  for(const book of myLibrary) {
+    const bookRow = document.createElement('tr');
+    for(const prop in book) {
+      const tableData = document.createElement('td');
+      tableData.textContent = book[prop];
+      bookRow.appendChild(tableData);
+      document.querySelector('.library-table tbody').appendChild(bookRow);
+    }
+  }
 }
