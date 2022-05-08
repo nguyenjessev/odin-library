de7ba1fc8198bbd5cbb6200525e4205b1f5c9cd5
Jesse Nguyen <nguyenjessev@gmail.com>
2022-05-08 12:57:33 -0400

    Add button to delete books from library

--- library.js
+++ library.js
@@ -15,14 +15,28 @@ function addBookToLibrary(title, author, pages, read) {
 
 function refreshBooks() {
   document.querySelector('.library-table tbody').innerHTML = null
-  for(const book of myLibrary) {
+  for(const [i, book] of myLibrary.entries()) {
     const bookRow = document.createElement('tr');
     for(const prop in book) {
       const tableData = document.createElement('td');
       tableData.textContent = book[prop];
       bookRow.appendChild(tableData);
-      document.querySelector('.library-table tbody').appendChild(bookRow);
     }
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('type', 'button');
+    deleteButton.setAttribute('class', 'delete-button');
+    deleteButton.setAttribute('data-index', i);
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      myLibrary.splice(i, 1);
+      refreshBooks();
+    })
+
+    const deleteButtonTd = document.createElement('td');
+    deleteButtonTd.appendChild(deleteButton);
+    bookRow.appendChild(deleteButtonTd);
+
+    document.querySelector('.library-table tbody').appendChild(bookRow);
   }
 }
 
