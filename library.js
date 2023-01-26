9385d8e3c2762b86ddfc5d3ce74e03909b9ab90f
Jesse Nguyen <nguyenjessev@gmail.com>
2023-01-26 12:44:25 -0500

    Check form validity before submitting book

--- library.js
+++ library.js
@@ -5,7 +5,7 @@ class Book {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read
+    this.read = read;
   }
 }
 
@@ -16,50 +16,53 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function refreshBooks() {
-  document.querySelector('.library-table tbody').innerHTML = null
-  for(const [i, book] of myLibrary.entries()) {
-    const bookRow = document.createElement('tr');
-    for(const prop in book) {
-      const tableData = document.createElement('td');
+  document.querySelector(".library-table tbody").innerHTML = null;
+  for (const [i, book] of myLibrary.entries()) {
+    const bookRow = document.createElement("tr");
+    for (const prop in book) {
+      const tableData = document.createElement("td");
       tableData.textContent = book[prop];
       bookRow.appendChild(tableData);
     }
 
-    const readToggleButton = document.createElement('button');
-    readToggleButton.setAttribute('type', 'button');
-    readToggleButton.setAttribute('class', 'read-toggle-button');
-    readToggleButton.setAttribute('data-index', i);
-    readToggleButton.innerText = 'Toggle Read Status';
-    readToggleButton.addEventListener('click', () => {
-      myLibrary[i].read = myLibrary[i].read == 'Yes' ? 'No' : 'Yes';
+    const readToggleButton = document.createElement("button");
+    readToggleButton.setAttribute("type", "button");
+    readToggleButton.setAttribute("class", "read-toggle-button");
+    readToggleButton.setAttribute("data-index", i);
+    readToggleButton.innerText = "Toggle Read Status";
+    readToggleButton.addEventListener("click", () => {
+      myLibrary[i].read = myLibrary[i].read == "Yes" ? "No" : "Yes";
       refreshBooks();
     });
-    const readToggleTd = document.createElement('td');
+    const readToggleTd = document.createElement("td");
     readToggleTd.appendChild(readToggleButton);
     bookRow.appendChild(readToggleTd);
 
-    const deleteButton = document.createElement('span');
-    deleteButton.setAttribute('class', 'delete-button');
-    deleteButton.setAttribute('data-index', i);
-    deleteButton.innerText = '❌';
-    deleteButton.addEventListener('click', () => {
+    const deleteButton = document.createElement("span");
+    deleteButton.setAttribute("class", "delete-button");
+    deleteButton.setAttribute("data-index", i);
+    deleteButton.innerText = "❌";
+    deleteButton.addEventListener("click", () => {
       myLibrary.splice(i, 1);
       refreshBooks();
     });
-    const deleteButtonTd = document.createElement('td');
+    const deleteButtonTd = document.createElement("td");
     deleteButtonTd.appendChild(deleteButton);
     bookRow.appendChild(deleteButtonTd);
 
-    document.querySelector('.library-table tbody').appendChild(bookRow);
+    document.querySelector(".library-table tbody").appendChild(bookRow);
   }
 }
 
-const addBookButton = document.querySelector('.new-book-section button');
-addBookButton.addEventListener('click', () => {
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  const pages = document.querySelector('#pages').value;
-  const read = document.querySelector('input[name="read"]:checked').value;
-  
-  addBookToLibrary(title, author, pages, read);
+const addBookButton = document.querySelector(".new-book-section button");
+addBookButton.addEventListener("click", () => {
+  const form = document.querySelector("form");
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const read = document.querySelector('input[name="read"]:checked');
+
+  if (form.reportValidity()) {
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
+  }
 });
